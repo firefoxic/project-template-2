464142da68c9773e6fa83041ba339046671f28d4
firefoxic <firefoxic.dev@gmail.com>
2022-07-27 11:57:36 +0300

    Remove all unnecessary

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -19,46 +19,6 @@ export default {
 		"mobile": null
 	},
 	"images": {
-		"product-hit": {
-			"ext": "jpg",
-			"width": 260,
-			"height": 315,
-			"maxdppx": 2,
-			"alt": "Три вязаные корзинки разных размеров.",
-			"viewports": ["desktop", "tablet", "mobile"]
-		},
-		"catalog-rabbit": {
-			"ext": "jpg",
-			"width": 260,
-			"height": 195,
-			"maxdppx": 2,
-			"alt": "Вязаный зайчик в голубом костюме.",
-			"viewports": ["desktop", "tablet", "mobile"]
-		},
-		"catalog-laundry-basket": {
-			"ext": "jpg",
-			"width": 260,
-			"height": 195,
-			"maxdppx": 2,
-			"alt": "Вязаная бело-коралловая корзинка с тремя наволочками внутри.",
-			"viewports": ["desktop", "tablet", "mobile"]
-		},
-		"catalog-toy-basket": {
-			"ext": "jpg",
-			"width": 260,
-			"height": 195,
-			"maxdppx": 2,
-			"alt": "Вязаная бело-салатовая корзинка с мягкими игрушками внутри.",
-			"viewports": ["desktop", "tablet", "mobile"]
-		},
-		"video": {
-			"ext": "jpg",
-			"width": 260,
-			"height": 150,
-			"maxdppx": 2,
-			"alt": "Вязаная корзинка мятного-серого цвета.",
-			"viewports": ["desktop", "tablet", "mobile"]
-		},
 		"map": {
 			"ext": "png",
 			"width": 320,
