4c52c89de38fd6362881ca85cb71d907940d581d
firefoxic <firefoxic.dev@gmail.com>
2023-05-24 12:39:36 +0000

    Deploying to gh-pages from @ firefoxic/project-template-2@6665f28d9cf8a57e2a917f0113fbd70c81db8cd0 🚀

---
+++ js/index.js
@@ -0,0 +1,1 @@
+let mainNav=document.querySelector(".main-nav"),mainNavToggle=document.querySelector(".main-nav__toggle");mainNav.classList.remove("main-nav--nojs"),mainNav.classList.replace("main-nav--opened","main-nav--closed"),mainNavToggle.addEventListener("click",(()=>{mainNav.classList.toggle("main-nav--closed"),mainNav.classList.toggle("main-nav--opened")}));
